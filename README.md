2b4806cf2a97ebd447ff5acb6adc1aa1a511f04c
Emanuel Sommer <emanuel.sommer@web.de>
2025-07-09 01:06:14 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# BeansAndMeans
-Beans &amp; Means - Coffee Date matching @ LMU Institute of Statistics
+# Beans &amp; Means - Coffee Date matching @ LMU Institute of Statistics
+
+**Sign in like this:**
+
+1) Open: https://docs.google.com/spreadsheets/d/16lh2Ta_ZVLKOXfN-Z4_369_zkxPGWx2kUHipjnsBHbw/edit?usp=sharing
+2) Add your full name and email address to the `ActiveParticipants` sheet.
+3) Add your full name also to the `PastMatches` sheet and add people you already met/do not want to meet in a comma-separated way (no extra spaces) into the `PastMatches` column. In most cases you will just leave it empty.
+4) You're all set! You will be matched with someone else in the next cycle.
+
+**Once signed up the following is the process:**
+
+1) Every once in a while (usually every month) you will be randomly matched with another participant.
+2) I will send out an email that the new matches are available.
+3) Check out the `Matches` sheet to see who you are matched with.
+4) Get in touch with your match and arrange a coffee date with your match (no pressure, if you don't have time this cycle or don't want to meet - up to you). Everything is possible be it Zoom, in person, or going for a walk together - the general suggestion would be to keep it rather short (around 30 minutes) and casual.
+5) If want to be rematched or have any questions, let me know :)
---
+++ main.R
@@ -0,0 +1,56 @@
+library(googlesheets4)
+
+ss = "https://docs.google.com/spreadsheets/d/16lh2Ta_ZVLKOXfN-Z4_369_zkxPGWx2kUHipjnsBHbw/edit?usp=sharing"
+
+participants <- read_sheet(ss = ss, sheet = "ActiveParticipants")
+last_matches <- read_sheet(ss = ss, sheet = "Matches")
+past_matches <- read_sheet(ss = ss, sheet = "PastMatches")
+
+active_names <- participants$Name
+
+past_matches_list <- strsplit(past_matches$PastMatches, ",")
+names(past_matches_list) <- past_matches$Name
+
+for (name in names(past_matches_list)) {
+  if (name %in% last_matches$Name1) {
+    other_name <- last_matches$Name2[last_matches$Name1 == name]
+  } else if (name %in% last_matches$Name2) {
+    other_name <- last_matches$Name1[last_matches$Name2 == name]
+  } else {
+    next
+  }
+  if (!(other_name %in% past_matches_list[[name]])) {
+    past_matches_list[[name]] <- ifelse(
+      is.na(past_matches_list[[name]]),
+      other_name,
+      c(past_matches_list[[name]], other_name)
+    )
+  }
+}
+
+# The main matching comes now:
+set.seed(2)
+new_matches <- data.frame(Name1 = character(), Name2 = character(), stringsAsFactors = FALSE)
+while (length(active_names) > 1) {
+  name1 <- sample(active_names, 1)
+  active_names <- active_names[active_names != name1]
+  feasible_candidates <- active_names[!(active_names %in% past_matches_list[[name1]])]
+  if (length(feasible_candidates) == 0) {
+    next
+  }
+  name2 <- sample(feasible_candidates, 1)
+  active_names <- active_names[active_names != name2]
+  new_matches <- rbind(new_matches, data.frame(Name1 = name1, Name2 = name2))
+}
+
+# Check the results:
+if (length(active_names) == 1) {
+  print(paste("! Unmatched participant:", active_names))
+}
+new_matches
+
+# Overwrite the sheets
+new_past_matches <- data.frame(Name = names(past_matches_list), PastMatches = sapply(past_matches_list, function(x) paste(x, collapse = ",")), stringsAsFactors = FALSE)
+rownames(new_past_matches) <- NULL
+write_sheet(new_matches, ss = ss, sheet = "Matches")
+write_sheet(new_past_matches, ss = ss, sheet = "PastMatches")
